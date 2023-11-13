98044edc5c187047ac738c451618846e67970bb5
theComputerNerd <50527002+ItzNaeem@users.noreply.github.com>
2023-11-13 14:18:20 +0300

    update contact form

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -1,14 +1,13 @@
 function sendToWhatsapp(){
 	let number = "+254712350742";
-
+	let website = 'https://cakeologywithjumana.com/';
 	let name = document.getElementById('name').value;
-	let email = document.getElementById('email').value;
 	let message = document.getElementById('message').value;
 
 	var url = "https://wa.me/" + number + "?text="
+	+ "Website : " +website+ "%0a%0a"
 	+ "Name : " +name+ "%0a"
-	+ "Email : " +email+ "%0a"
-	+ "Message : " +message+ "%0a%0a";
+	+ "Message : " +message;
 
 	window.open(url, '_blank').focus();
 }
